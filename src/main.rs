d907d326e66db722a74fd1f3dd9738e3184a0e9d
jsulz <j.sulzdorf@gmail.com>
2025-07-06 14:17:09 -0700

    wiring in dependencies and adding initial commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+use clap::Subcommand;
+
+// Implement with subcommand since this is nested under the top-level enum
+#[derive(Debug, Subcommand)]
+#[derive(Clone)]
+enum Commands {
+    Allow {
+        path: PathBuf
+    },
+    List,
+    Install,
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct CLI {
+    // this field is a subcommand
+    #[command(subcommand)] 
+    command: Commands
+}
+
+
 fn main() {
-    println!("Hello, hugit!");
+    let args = CLI::parse();
 }
