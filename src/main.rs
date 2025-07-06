76f0e1854672e39cb9de611afcf59ca386d049fd
jsulz <j.sulzdorf@gmail.com>
2025-07-06 14:46:44 -0700

    starting to work on config

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+use std::path::Path;
 use std::path::PathBuf;
 
+// Clap helpers
 use clap::Parser;
 use clap::Subcommand;
+
+// File helpers
+use directories::BaseDirs;
+use std::fs::File;
+use std::io::Write;
+use std::io::prelude::*;
 
 // Implement with subcommand since this is nested under the top-level enum
 #[derive(Debug, Subcommand)]
@@ -23,6 +31,11 @@ struct CLI {
 }
 
 
-fn main() {
-    let args = CLI::parse();
+fn main() -> Result<(), std::io::Error>{
+    if let Some(base_dirs) = BaseDirs::new() {
+        let dir: &Path = base_dirs.config_dir();
+        let mut file = File::create_new(dir.join("hugit-hooks.toml"))?;
+        file.write_all(b"allow_repos = []");
+    }
+    Ok(())
 }
