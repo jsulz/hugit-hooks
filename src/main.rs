4133aebec99285dea57e0ffb1697a6bb48a6406c
jsulz <j.sulzdorf@gmail.com>
2025-07-07 20:44:20 -0700

    continuing to work on config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// Path helpers
 use std::path::Path;
 use std::path::PathBuf;
 
@@ -6,10 +7,14 @@ use clap::Parser;
 use clap::Subcommand;
 
 // File helpers
-use directories::BaseDirs;
+use directories::ProjectDirs;
 use std::fs::File;
+use std::fs::read_to_string;
 use std::io::Write;
 use std::io::prelude::*;
+
+use serde::Serialize;
+use serde::Deserialize;
 
 // Implement with subcommand since this is nested under the top-level enum
 #[derive(Debug, Subcommand)]
@@ -30,12 +35,46 @@ struct CLI {
     command: Commands
 }
 
+#[derive(Deserialize, Serialize)]
+struct Config {
+    allowed_repos: Vec<PathBuf>
+}
 
-fn main() -> Result<(), std::io::Error>{
-    if let Some(base_dirs) = BaseDirs::new() {
-        let dir: &Path = base_dirs.config_dir();
-        let mut file = File::create_new(dir.join("hugit-hooks.toml"))?;
-        file.write_all(b"allow_repos = []");
+impl Config {
+    fn load(&self) -> Result<Config, Box<dyn std::error::Error>>{
+        let dir: ProjectDirs = ProjectDirs::from("com", "jsulz", "hugit-hooks")
+            .ok_or_else(|| anyhow::anyhow!("could not determine config directory"))?;
+        let config_path: PathBuf = dir.config_dir().join("config.toml");
+
+        match read_to_string(&config_path) {
+            Ok(conf_contents) => {
+                // Try to read from the toml file into the a config struct and return
+                match toml::from_str::<Config>(&conf_contents) {
+                    Ok(config) => return Ok(config),
+                    Err(e) => return Ok(Config{ allowed_repos: vec![PathBuf::new()]})
+
+                }
+            }
+            // An error here means this file doesn't exist and we should just return conf
+            Err(e) => return Ok(Config{ allowed_repos: vec![PathBuf::new()]})
+        }
     }
-    Ok(())
+    
+    fn save(&self){
+
+    }
+
+    fn is_allowed(&self, repo_path: PathBuf){
+
+    }
+
+    fn add_repo(&self, repo_path: PathBuf){
+
+    }
+}
+
+
+fn main() {
+    let conf = Config{ allowed_repos: vec![PathBuf::new()]};
+    conf.load();
 }
